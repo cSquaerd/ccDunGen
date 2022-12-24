283ae44f1bd875c1d70f163ecd078be0fe73b94a
Charles Patterson Cook <cook.char.p@gmail.com>
2022-12-23 22:49:58 -0500

    Added hall generation and more draw modes to Catacombs

--- ccDGLevels.py
+++ ccDGLevels.py
@@ -1,5 +1,6 @@
 from ccDGGeom import np, Point, Rectangle, Line, Circle
 
+# Nethack style dungeon
 class Catacombs:
 	def __init__(
 		self, w : int, h : int,
@@ -14,12 +15,13 @@ class Catacombs:
 		self.roomAvgAreaPercent = raap
 		self.variance = Point(varix, variy)
 		self.padding = Point(padx, pady)
-		
+		# Average dimension of each room
 		adim = (self.size.npar * np.sqrt(self.roomAvgAreaPercent)).astype(int)
 		self.roomAvgDim = Point(adim[1], adim[0])
-		
+		# Store the rooms and halls in these lists, must generate them separately
 		self.rooms = []
 		self.halls = []
+		self.hallCounts = []
 		
 	def __str__(self) -> str:
 		return (
@@ -40,18 +42,19 @@ class Catacombs:
 	
 	def __repr__(self) -> str:
 		return self.__str__()
-	
+	# Randomly generate rooms	
 	def genRooms(self, reset = False, attemptsOverride : int = 0):
 		if not reset:
 			return
 		
 		self.rooms = []
+		self.halls = []
 		attempts = 0
 		if attemptsOverride > 0:
 			maxAttempts = attemptsOverride
-		else:
-			maxAttempts = int(
-				round(
+		else: # $$ n_{rooms} \times \max(x_{pad} * y_{pad}, 1)
+			maxAttempts = int( # \times 10^{-\ln(1 - n_{rooms} \times a_{avg})}
+				round( # \times e^{1 + n_{rooms} \times a_{avg}} $$
 					self.roomCount * max(self.padding.x * self.padding.y, 1) \
 					* 10. ** (
 						-np.log(1. - self.roomCount * self.roomAvgAreaPercent)
@@ -98,14 +101,133 @@ class Catacombs:
 					print("Warning: maximum room generation attempts reached.")
 					print("Your dungeon will only have", len(self.rooms), "rooms")
 				break
+
+		self.hallCounts = [0 for i in range(len(self.rooms))]
 		
 		print("Attemped room generations", attempts, "times.")
+
+	def genHalls(self, reset : bool = False):
+		if not reset:
+			return
+	
+		self.halls = []
+
+		for i in range(len(self.rooms)):
+			room = self.rooms[i]
+
+			distances = [
+				[j, room.getCentroid() | self.rooms[j].getCentroid()]
+				for j in range(len(self.rooms))
+			]
+			distances[i][1] = 2 * max([t[1] for t in distances])
+			distances = sorted(distances, key = lambda t : t[1])[:len(distances) - 1]
+			print(distances)
+
+			j = 0
+			#for otherInfo in distances[:self.hallAvgCount - self.hallCounts[i]]:
+			while self.hallCounts[i] < self.hallAvgCount:
+				k = distances[j][0]
+				mhDist = distances[j][1]
+				other = self.rooms[k]
+
+				wallInfo = room.getNearestWall(other)
+				wallOrient = wallInfo[0]
+				wallCells = wallInfo[1]
+
+				wallOtherInfo = other.getNearestWall(room)
+				wallOtherOrient = wallOtherInfo[0]
+				wallOtherCells = wallOtherInfo[1]
+
+				print(i, j, k, room.getAzimuth(other), wallOrient, len(wallCells))
+
+				startRoom = list(wallCells)[
+					np.random.randint(0, len(wallCells))
+				]
+				startOther = list(wallOtherCells)[
+					np.random.randint(0, len(wallOtherCells))
+				]
 				
-	def draw(self) -> np.array:
-		mask = np.zeros(self.size.npar, bool)
+				delta = startRoom - startOther
+				dx = abs(delta.x)
+				dy = abs(delta.y)
+
+				print(startRoom, startOther, dx, dy)
+
+				if wallOrient in ('n', 's') and wallOtherOrient in ('n', 's') \
+					or wallOrient in ('e', 'w') and wallOtherOrient in ('e', 'w'):				
+					roomHall = Line(
+						startRoom.x, startRoom.y,
+						dx // 2 + 1 + dx % 2 if wallOrient in ('e', 'w')
+						else dy // 2 + 1 + dy % 2,
+						wallOrient
+					)
+					otherHall = Line(
+						startOther.x, startOther.y,
+						dx // 2 + 1 if wallOtherOrient in ('e', 'w')
+						else dy // 2 + 1,
+						wallOtherOrient
+					)
+
+					connectingOrientation = roomHall.getNearestOrientation(
+						otherHall, mode = 'e'
+					)[0]
+					connectingHall = Line(
+						roomHall.getEndpoint().x, roomHall.getEndpoint().y,
+						dy + 1 if wallOrient in ('e', 'w') else dx + 1,
+						connectingOrientation
+					)
+
+					self.halls.append(connectingHall)
+				else: # Parallel above, Perpendicular below
+					print("NON-CONNECTION HALL GENERATED!!!")
+					roomHall = Line(
+						startRoom.x, startRoom.y,
+						dx + 1 if wallOrient in ('e', 'w') else dy + 1,
+						wallOrient
+					)
+					
+					otherHall = Line(
+						startOther.x, startOther.y,
+						dx + 1 if wallOtherOrient in ('e', 'w') else dy + 1,
+						wallOtherOrient
+					)
+
+				self.halls.append(roomHall)
+				self.halls.append(otherHall)
+
+				self.hallCounts[i] += 1
+				self.hallCounts[k] += 1
+				j += 1
+				j %= len(distances)
+				
+	def draw(self, mode : str = '') -> np.array:
+		maskRoomEdge = np.zeros(self.size.npar, bool)
 		for r in self.rooms:
-			mask |= r.getMaskEdge(self.size.x, self.size.y)
+			maskRoomEdge |= r.getMaskEdge(self.size.x, self.size.y)
+
+		maskRoomFill = np.zeros(self.size.npar, bool)
+		for r in self.rooms:
+			maskRoomFill |= r.getMaskFill(self.size.x, self.size.y)
+
+		maskHall = np.zeros(self.size.npar, bool)
 		for h in self.halls:
-			mask |= h.getMask(self.size.x, self.size.y)
+			maskHall |= h.getMask(self.size.x, self.size.y)
+
+		if mode.upper() == "HALLONLY":
+			return maskHall
+		elif mode.upper() == "NOWALLS":
+			return maskHall | (
+				maskRoomFill & ~ maskRoomEdge
+			)
+		elif mode.upper() == "DOORS" or mode.upper() == "LIZARDKING":
+			return (
+				maskHall & maskRoomEdge
+			) | (
+				maskRoomFill & ~ maskRoomEdge
+			)
 			
-		return mask
+		return maskRoomEdge | maskHall & ~ (
+			maskRoomFill & maskHall & ~ (
+				maskRoomEdge & maskHall
+			)
+		)
